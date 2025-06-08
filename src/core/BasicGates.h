c8db505c3c27629c2b659ac16bd68d31836fd52b
Abhinav singh <asingh308@nait.ca>
2025-06-08 15:30:56 -0600

    basic gates header files completed

--- src/core/BasicGates.h
+++ src/core/BasicGates.h
@@ -1,23 +1,62 @@
 #pragma once
 #include "Gate.h"
+#include <stdexcept>
 
 class ANDGate : public Gate
 {
-    // TODO: Constructor calling base class
-    // TODO: Override evaluate() method
+public:
+    ANDGate(const std::string &gateLabel = "") : Gate(GateType::And, gateLabel) {}
+    void evaluate() override {}
 };
 
 class ORGate : public Gate
 {
-    // TODO: Constructor calling base class
-    // TODO: Override evaluate() method
+public:
+    ORGate(const std::string &gateLabel = "") : Gate(GateType::Or, gateLabel) {}
+    void evaluate() override {}
 };
 
 class NOTGate : public Gate
 {
-    // TODO: Constructor with single input validation
-    // TODO: Override evaluate() method
-    // TODO: Add input count restriction
+public:
+    NOTGate(const std::string &gateLabel = "") : Gate(GateType::Not, gateLabel) {}
+    void evaluate() override {}
+
+private:
+    void addInputCountRestriction() {}
 };
 
-// TODO: Implement remaining 5 gate types (NAND, NOR, XOR, XNOR, BUFFER)
+class NANDGate : public Gate
+{
+public:
+    NANDGate(const std::string &gateLabel = "") : Gate(GateType::Nand, gateLabel) {}
+    void evaluate() override {}
+};
+
+class NORGate : public Gate
+{
+public:
+    NORGate(const std::string &gateLabel = "") : Gate(GateType::Nor, gateLabel) {}
+    void evaluate() override {}
+};
+
+class XORGate : public Gate
+{
+public:
+    XORGate(const std::string &gateLabel = "") : Gate(GateType::Xor, gateLabel) {}
+    void evaluate() override {}
+};
+
+class XNORGate : public Gate
+{
+public:
+    XNORGate(const std::string &gateLabel = "") : Gate(GateType::Xnor, gateLabel) {}
+    void evaluate() override {}
+};
+
+class BufferGate : public Gate
+{
+public:
+    BufferGate(const std::string &gateLabel = "") : Gate(GateType::Buffer, gateLabel) {}
+    void evaluate() override {}
+};
